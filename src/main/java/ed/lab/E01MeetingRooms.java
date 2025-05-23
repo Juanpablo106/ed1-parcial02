f0fd5b43603d34579167bfad951f4eee38fc891b
Juan Pablo <juanpaestra8@gmail.com>
2025-05-23 08:05:06 -0600

    Parcial 02 - Juan Pablo Estrada Rodríguez 1056124

--- src/main/java/ed/lab/E01MeetingRooms.java
+++ src/main/java/ed/lab/E01MeetingRooms.java
@@ -1,11 +1,27 @@
 package ed.lab;
 
-import java.util.List;
+import java.util.*;
 
 public class E01MeetingRooms {
 
     public int minMeetingRooms(List<MeetingInterval> meetingIntervals) {
-        return -1;
-    }
+        if (meetingIntervals == null || meetingIntervals.isEmpty()) {
+            return 0;
+        }
 
+        meetingIntervals.sort(Comparator.comparingInt(MeetingInterval::startTime));
+
+        PriorityQueue<Integer> endTimes = new PriorityQueue<>();
+
+        for (MeetingInterval interval : meetingIntervals) {
+
+            if (!endTimes.isEmpty() && endTimes.peek() <= interval.startTime()) {
+                endTimes.poll(); // Libera la sala
+            }
+
+            endTimes.offer(interval.endTime());
+        }
+
+        return endTimes.size();
+    }
 }
--- src/main/java/ed/lab/E02Logger.java
+++ src/main/java/ed/lab/E02Logger.java
@@ -1,12 +1,23 @@
 package ed.lab;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class E02Logger {
 
+    private final Map<String, Integer> lastTimestamp;
+
     public E02Logger() {
+        this.lastTimestamp = new HashMap<>();
     }
-
     public boolean shouldPrintMessage(int timestamp, String message) {
-        return false;
+        if (lastTimestamp.containsKey(message)) {
+            int lastTime = lastTimestamp.get(message);
+            if (timestamp - lastTime < 10) {
+                return false;
+            }
+        }
+        lastTimestamp.put(message, timestamp);
+        return true;
     }
-
 }
